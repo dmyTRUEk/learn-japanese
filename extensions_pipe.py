52387007ebd0415a176465b65705444216466fa4
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2022-05-11 18:29:50 +0300

    micro cleanup

--- extensions_pipe.py
+++ extensions_pipe.py
@@ -6,7 +6,6 @@ from pipe import Pipe, to_type
 
 
 
-# TODO?: fix `abs_` instead of `abs` due to some bug?
 abs_ = Pipe(lambda x: abs(x))
 min_ = lambda m: Pipe(lambda x: min(m, x))
 max_ = lambda m: Pipe(lambda x: max(m, x))
@@ -14,7 +13,6 @@ max_ = lambda m: Pipe(lambda x: max(m, x))
 to_str = to_type(str)
 to_int = to_type(int)
 to_float = to_type(float)
-#to_int = Pipe(lambda x: int(x))
 
 to_int_round = Pipe(lambda x: round(x))
 
