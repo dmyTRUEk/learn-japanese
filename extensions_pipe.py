6838f90080812b1b656526ae5c556759470a033b
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2022-05-13 16:38:54 +0300

    add flatten

--- extensions_pipe.py
+++ extensions_pipe.py
@@ -16,3 +16,14 @@ to_float = to_type(float)
 
 to_int_round = Pipe(lambda x: round(x))
 
+def _flatten(l: list) -> list:
+    res = []
+    for el in l:
+        if type(el) == list:
+            res += _flatten(el)
+        else:
+            res.append(el)
+    return res
+
+flatten = Pipe(lambda l: _flatten(l))
+
